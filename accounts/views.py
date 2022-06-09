c8b19998daaecb49fc3480e632336a46493e32e7
azadranjith <104302859+azadranjith@users.noreply.github.com>
2022-06-09 21:25:24 +0530

    Update views.py
    email service commented at register

--- accounts/views.py
+++ accounts/views.py
@@ -54,22 +54,22 @@ def register(request):
 
             #user activation
 
-            current_site = get_current_site(request)
+#             current_site = get_current_site(request)
 
-            mail_subject = 'Please activate your account'
+#             mail_subject = 'Please activate your account'
 
-            message = render_to_string('accounts/account_verification_email.html',{
-                'user':user,
-                'domain':current_site,
-                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
-                'token':default_token_generator.make_token(user),
-            })
+#             message = render_to_string('accounts/account_verification_email.html',{
+#                 'user':user,
+#                 'domain':current_site,
+#                 'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+#                 'token':default_token_generator.make_token(user),
+#             })
 
-            to_email = email
+#             to_email = email
 
-            send_email = EmailMessage(mail_subject,message,to=[to_email])
+#             send_email = EmailMessage(mail_subject,message,to=[to_email])
 
-            send_email.send()   
+#             send_email.send()   
 
             
             messages.success(request,'thank you for registering please verify your email')
