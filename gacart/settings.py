e7adea6503d43f5cf2951dd69872079b6d25ae45
ranjith <azadranjith@gmail.com>
2022-06-09 08:01:06 +0530

    whitenoiseadded

--- gacart/settings.py
+++ gacart/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,7 +141,7 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'gaadmin/static/'),os.path.join(BASE_DIR,'gacart/static')  
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # media file configuration in database
 
