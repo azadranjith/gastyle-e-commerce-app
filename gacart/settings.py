1f8dd1227241274bfee28945a362bdb098d8069b
azadranjith <104302859+azadranjith@users.noreply.github.com>
2022-06-23 20:54:47 +0530

    Update settings.py

--- gacart/settings.py
+++ gacart/settings.py
@@ -141,17 +141,28 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl':'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = True
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR,'gaadmin/static/'),os.path.join(BASE_DIR,'gacart/static')  
+STATICFILES_DIRS=[
+    'static',
 ]
+STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'gacart.media_storages.MediaStorage'
+# STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR,'gaadmin/static/'),os.path.join(BASE_DIR,'gacart/static')  
+# ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # media file configuration in database
 
